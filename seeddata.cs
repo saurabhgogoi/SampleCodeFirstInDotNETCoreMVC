1d26a34e61307902b2f966ef0591044349bd8a1c
saurabhgogoi <101546679+saurabhgogoi@users.noreply.github.com>
2022-04-11 16:52:48 +0530

    Add files via upload

---
+++ seeddata.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SampleCodeFirstInDotNETCoreMVC.Migrations
+{
+    public partial class seeddata : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "TodoItem",
+                columns: new[] { "Id", "Description", "IsComplete", "Name" },
+                values: new object[] { 1L, "In meeting need to discuss some points.", true, "Meeting with management" });
+
+            migrationBuilder.InsertData(
+                table: "TodoItem",
+                columns: new[] { "Id", "Description", "IsComplete", "Name" },
+                values: new object[] { 2L, "List of thus this item buy.", false, "Go for shopping" });
+
+            migrationBuilder.InsertData(
+                table: "TodoItem",
+                columns: new[] { "Id", "Description", "IsComplete", "Name" },
+                values: new object[] { 3L, "Here is task to ask to do on call", true, "Call to some one for do some task" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "TodoItem",
+                keyColumn: "Id",
+                keyValue: 1L);
+
+            migrationBuilder.DeleteData(
+                table: "TodoItem",
+                keyColumn: "Id",
+                keyValue: 2L);
+
+            migrationBuilder.DeleteData(
+                table: "TodoItem",
+                keyColumn: "Id",
+                keyValue: 3L);
+        }
+    }
+}
